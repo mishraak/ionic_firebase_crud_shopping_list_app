007044ea246dcbd13fec8a7acb025f93257c2aa5
mishraak <mishra.akshay91@gmail.com>
2018-03-15 01:05:09 -0700

    ionic app with firebase backend

--- src/app/firebase.creds.ts
+++ src/app/firebase.creds.ts
@@ -1,8 +1,8 @@
 export const FIREBASE_CREDENTIALS = {
-    apiKey: "AIzaSyDX32WP14bKqRM8ush6RCk5AsKNCQ7X2eI",
-    authDomain: "fir-crud-4b417.firebaseapp.com",
-    databaseURL: "https://fir-crud-4b417.firebaseio.com",
-    projectId: "fir-crud-4b417",
-    storageBucket: "fir-crud-4b417.appspot.com",
-    messagingSenderId: "795359058959"
+    apiKey: "FIREBASE_API_KEY",
+    authDomain: "FIREBASE_AUTH_DOMAIN",
+    databaseURL: "DATABASE_URL",
+    projectId: "PROJECT_ID",
+    storageBucket: "STORAGE_BUCKET",
+    messagingSenderId: "MESSEGINGSENDERID"
 };
